89145a93322b80a772ec01973c4ef939a02f3645
cva2019 <124920750+cva2019@users.noreply.github.com>
2025-04-15 21:38:01 +0700

    Create WeatherDailyDetail.kt

---
+++ app/src/main/java/com/example/weatherapp/WeatherDailyDetail.kt
@@ -0,0 +1,26 @@
+package com.example.weatherapp
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "weather_daily_detail",
+    foreignKeys = [ForeignKey(
+        entity = WeatherData::class,
+        parentColumns = ["id"],
+        childColumns = ["weatherDataId"],
+        onDelete = ForeignKey.CASCADE
+    )],
+    indices = [Index(value = ["weatherDataId"])]
+)
+data class WeatherDailyDetail(
+    @PrimaryKey(autoGenerate = true) val detailId: Long = 0,
+    val weatherDataId: Long,
+    val time: String,
+    val temperature_2m_max: Double,
+    val temperature_2m_min: Double,
+    val precipitation_probability_max: Double,
+    val weather_code: Int
+)
